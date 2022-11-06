92684e4ecf74f7358ca0e7837d5cadb27ecd2e81
ErickProgramer <115942865+ErickProgramer@users.noreply.github.com>
2022-11-05 22:53:55 -0300

    utils
    the file is used to align the dataframe and series.

---
+++ utils/strings.lua
@@ -0,0 +1,33 @@
+local str = {}
+
+--@param s       string
+--@param spaces  integer
+--@param concat? string
+function str.left(s, spaces, concat)
+  concat = concat or ' '
+  local align = #s - spaces
+  if align < 0 then align = align * -1 end
+
+  local st = ''
+  for _ = 1, spaces do
+    st = st .. concat
+  end
+  return st..s
+end
+
+--@param s       string
+--@param spaces  integer
+--@param concat? string
+function str.right(s, spaces, concat)
+  concat = concat or ' '
+  local align = #s - spaces
+  if align < 0 then align = align * -1 end
+
+  local st = ''
+  for _ = 1, spaces do
+    st = st .. concat
+  end
+  return s..st
+end
+
+return str
